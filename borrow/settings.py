91c395f6fd52270b908063356faba116f134a133
Piyush Khandelwal <55063039+piyush-pk@users.noreply.github.com>
2021-07-08 21:05:21 +0530

    Update settings.py
    Admin mail added

--- borrow/settings.py
+++ borrow/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'hackbypk@gmail.com'
-EMAIL_HOST_PASSWORD = 'Piyush@1202'
+EMAIL_HOST_USER = 'telecomthakuriya@gmail.com'
+EMAIL_HOST_PASSWORD = 'Anand@8946'
